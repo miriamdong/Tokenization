8e570ad18d4fae165e992374c9c73ca8d714bfe1
Miriam Dong <miriamdong@gmail.com>
2021-05-10 13:46:39 -0700

    init

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+const MyToken = artifacts.require("./MyToken.sol");
+const MyTokenSales = artifacts.require("./MyTokenSale.sol");
+
+module.exports = async function (deployer) {
+	let addr = await web3.eth.getAccounts();
+	await deployer.deploy(MyToken, 1000000000);
+	await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
+	let tokenInstance = await MyToken.deployed();
+	await tokenInstance.transfer(MyTokenSales.address, 1000000000);
+};
